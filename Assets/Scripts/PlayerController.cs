d5547959fc40bfc36e0a4674a5ad067642cc36e9
Pierre-Yves Jourel <pierre-yves-eric.jourel@polymtl.ca>
2018-11-24 15:39:02 -0500

    Add support for Android Devices

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,10 @@
-﻿        using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class PlayerController : NetworkBehaviour {
+public class PlayerController : NetworkBehaviour
+{
 
     // Use this for initialization
     void Start()
@@ -12,37 +13,54 @@ public class PlayerController : NetworkBehaviour {
     }
 
     [Command]
-    void CmdSendSpaceDown() {
+    void CmdSendSpaceDown()
+    {
         Debug.Log("SPACE DOWN RPC from : " + this.netId);
         CookieController.singleton.spaceDownFor(int.Parse(this.netId.ToString()));
     }
 
     [Command]
-    void CmdSendSpaceUp() {
+    void CmdSendSpaceUp()
+    {
         Debug.Log("SPACE UP RP from : " + this.netId);
         CookieController.singleton.spaceUpFor(int.Parse(this.netId.ToString()));
     }
 
 
 
-	// Update is called once per frame
-    void Update () {
-        if (!isLocalPlayer) {
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
             return;
         }
         checkInput();
-	}
+    }
 
     // Get if space is down
-    void checkInput() {
+    void checkInput()
+    {
+#if UNITY_STANDALONE || UNITY_STANDALONE_OSX
         if (Input.GetKeyDown(KeyCode.Space))
         {
             CmdSendSpaceDown();
         }
 
-        if (Input.GetKeyUp(KeyCode.Space)) {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
             CmdSendSpaceUp();
         }
 
+#elif UNITY_IOS || UNITY_ANDROID
+        Touch touch = Input.GetTouch(0);
+        if(touch.phase == TouchPhase.Began) {
+            CmdSendSpaceDown();
+        }
+
+        if(touch.phase == TouchPhase.Ended) {
+            CmdSendSpaceUp();
+        }
+#endif
     }
 }
