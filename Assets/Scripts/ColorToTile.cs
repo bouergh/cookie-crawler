03bec84d60e926416e439200fa6e0326d0d92e8e
Ro se <rosehirigoyen@gmail.com>
2018-11-24 13:15:56 -0500

    RH: Player moving on map with collisions

---
+++ Assets/Scripts/ColorToTile.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[System.Serializable]
+public class ColorToTile
+{
+    public Color color;
+    public Tile tile;
+    public int map; // 0: base, 1: collision
+}
---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapGenerator : MonoBehaviour {
+
+    public Texture2D map;
+    public ColorToTile[] colorMappings;
+    public Tilemap baseMap;
+    public Tilemap collisionMap;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("START");
+        GenerateLevel();
+	}
+	
+    void GenerateLevel()
+    {
+        for (int x = 0; x < map.width; x++)
+        {
+            for (int y = 0; y < map.height; y++)
+            {
+                GenerateTile(x, y);
+            }
+        }
+    }
+
+    void GenerateTile(int x, int y)
+    {
+        Color pixelColor = map.GetPixel(x, y);
+
+        foreach (ColorToTile colorMapping in colorMappings)
+        {
+            if (colorMapping.color.Equals(pixelColor))
+            {
+                if (colorMapping.map == 0)
+                    baseMap.SetTile(new Vector3Int(x, y, 0), colorMapping.tile);
+                else
+                    collisionMap.SetTile(new Vector3Int(x, y, 0), colorMapping.tile);
+                break;
+            }
+        }
+    }
+}
