39745bae417a5630c674523b37a5407f098473ae
Pierre-Yves Jourel <pierre-yves-eric.jourel@polymtl.ca>
2018-11-24 14:26:35 -0500

    Cookie choice action from network players

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿        using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Player create");
+    }
+
+    [Command]
+    void CmdSendSpaceDown() {
+        Debug.Log("SPACE DOWN RPC from : " + this.netId);
+        CookieController.singleton.spaceDownFor(int.Parse(this.netId.ToString()));
+    }
+
+    [Command]
+    void CmdSendSpaceUp() {
+        Debug.Log("SPACE UP RP from : " + this.netId);
+        CookieController.singleton.spaceUpFor(int.Parse(this.netId.ToString()));
+    }
+
+
+
+	// Update is called once per frame
+    void Update () {
+        if (!isLocalPlayer) {
+            return;
+        }
+        checkInput();
+	}
+
+    // Get if space is down
+    void checkInput() {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdSendSpaceDown();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            CmdSendSpaceUp();
+        }
+
+    }
+}
