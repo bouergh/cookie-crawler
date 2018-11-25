50eeb801322174a6c564949fa8b496a065c1dfef
Ro se <rosehirigoyen@gmail.com>
2018-11-24 19:46:59 -0500

    RH:Fireball

---
+++ Assets/Scripts/FireBall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class FireBall : MonoBehaviour {
+    public Tilemap traversableMap;
+    public GameObject player;
+	int speed = 10;
+    Vector3 dir;
+
+    void Start()
+    {
+        dir = Vector3.Normalize(player.transform.position - gameObject.transform.position);
+        transform.rotation = Quaternion.LookRotation(dir, Vector3.forward);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(dir.x * Time.deltaTime * speed, dir.y * Time.deltaTime, 0);
+
+        if (traversableMap.HasTile(Vector3Int.CeilToInt(gameObject.transform.position)))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour {
 
+    public GameObject toSpawn;
+    const int COOLDOWN = 10;
+    private int cpt = COOLDOWN;
+
+    void Update()
+    {
+        if (cpt >= COOLDOWN)
+        {
+            Spawn();
+            cpt = 0;
+        }
+        cpt++;
+    }
+
+    void Spawn()
+    {
+        Instantiate(toSpawn, gameObject.transform.position, Quaternion.identity);
+    }
 
 }
